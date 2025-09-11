cd764f33039e13530dd064a6be303578e0c37dc2
sandptel <sandeeppatel626735@gmail.com>
2025-09-11 22:40:08 +0530

    feat: adds write_all

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
+    println!("Logs:");
+    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
