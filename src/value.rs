dbc5671a6a7ca4639659f22290c3e350ab926ceb
sandptel <sandeeppatel626735@gmail.com>
2025-10-07 04:47:16 +0530

    codecrafters submit [skip ci]

---
+++ src/value.rs
@@ -0,0 +1,98 @@
+use std::collections::HashMap;
+
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RedisValue {
+    String(String),
+    List(Vec<String>),
+    Hash(HashMap<String, String>),
+}
+
+impl RedisValue {
+    pub fn from_string(value: String) -> RedisValue {
+        RedisValue::String(value)
+    }
+    pub fn from_list(list: Vec<String>) -> RedisValue {
+        RedisValue::List(list)
+    }
+    pub fn get_response(&self) -> String {
+        match self {
+            // Simple string value - return as bulk string
+            RedisValue::String(s) => { format!("${}\r\n{}\r\n", s.len(), s) }
+
+            // List - return as array of bulk strings
+            RedisValue::List(list) => {
+                if list.is_empty() {
+                    "*0\r\n".to_string()
+                } else {
+                    let mut response = format!("*{}\r\n", list.len());
+                    for item in list {
+                        response.push_str(&format!("${}\r\n{}\r\n", item.len(), item));
+                    }
+                    response
+                }
+            }
+
+            // Set - return as array of bulk strings
+            // RedisValue::Set(set) => {
+            //     if set.is_empty() {
+            //         "*0\r\n".to_string()
+            //     } else {
+            //         let mut response = format!("*{}\r\n", set.len());
+            //         for item in set {
+            //             response.push_str(&format!("${}\r\n{}\r\n", item.len(), item));
+            //         }
+            //         response
+            //     }
+            // }
+
+            // Hash - return as array of field-value pairs (flattened)
+            RedisValue::Hash(hash) => {
+                if hash.is_empty() {
+                    "*0\r\n".to_string()
+                } else {
+                    // Each key-value pair becomes 2 elements in the array
+                    let mut response = format!("*{}\r\n", hash.len() * 2);
+                    for (key, value) in hash {
+                        response.push_str(&format!("${}\r\n{}\r\n", key.len(), key));
+                        response.push_str(&format!("${}\r\n{}\r\n", value.len(), value));
+                    }
+                    response
+                }
+            }
+        }
+    }
+
+    // The TYPE command returns the type of value stored at a given key. These types include: string, list, set, zset, hash, stream, and vectorset.
+    // Server should respond with +string\r\n, which is string encoded as a simple string.
+    pub fn get_type_response(&self) -> String {
+        match self {
+            // Simple string value - return as bulk string
+            RedisValue::String(s) => { "+string\r\n".to_string() }
+
+            // List - return as array of bulk strings
+            RedisValue::List(list) => { "+list\r\n".to_string() }
+
+            // Set - return as array of bulk strings
+            // RedisValue::Set(set) => {
+            //     if set.is_empty() {
+            //         "*0\r\n".to_string()
+            //     } else {
+            //         let mut response = format!("*{}\r\n", set.len());
+            //         for item in set {
+            //             response.push_str(&format!("${}\r\n{}\r\n", item.len(), item));
+            //         }
+            //         response
+            //     }
+            // }
+
+            // Hash - return as array of field-value pairs (flattened)
+            RedisValue::Hash(hash) => { "+hash\r\n".to_string() }
+        }
+    }
+
+    /// Returns null response when key doesn't exist
+    pub fn get_null_response() -> String {
+        "$-1\r\n".to_string()
+    }
+}
